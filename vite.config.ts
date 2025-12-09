1cb471b1e3d50dcc05e78f72c31c5256519447c6
Henry Lin <henry.ys.lin@gmail.com>
2025-12-09 11:26:41 +0800

    Disallow direct access to ssr routes

--- vite.config.ts
+++ vite.config.ts
@@ -118,6 +118,7 @@ export default defineConfig({
       },
     ],
     rewrites: [
+      { source: '/ssr/(.*)', destination: '/' },
       { source: '/solve/:puzzleId', destination: '/ssr/solve/:puzzleId' },
       {
         source: '/api/preview/:type/:resourceId',
