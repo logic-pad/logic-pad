467a7626a307992e43d0418e6c755726f4f70c00
Henry Lin <henry.ys.lin@gmail.com>
2025-12-09 11:25:43 +0800

    Log error

--- src/ssr/index.ts
+++ src/ssr/index.ts
@@ -7,6 +7,12 @@ import { image } from './routes/image';
 // TODO: A dirty hack to get around import.meta.env being undefined in vite-plugin-vercel v9
 axios.defaults.baseURL = process.env.VITE_API_ENDPOINT;
 
-const app = new Elysia().use(sitemap).use(ssr).use(image);
+const app = new Elysia()
+  .onError(({ error }) => {
+    console.error(error);
+  })
+  .use(sitemap)
+  .use(ssr)
+  .use(image);
 
 export default app;
