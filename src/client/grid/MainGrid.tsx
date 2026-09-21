10ebe54bdb222017264a1c1d0e64f1b6af0dfa37
Henry Lin <henry.ys.lin@gmail.com>
2026-09-21 20:47:15 +0800

    Add bottom margin

--- src/client/grid/MainGrid.tsx
+++ src/client/grid/MainGrid.tsx
@@ -163,7 +163,7 @@ export default memo(function MainGrid({
       height={grid.height}
       allowAnimation={allowAnimation}
       {...bind()}
-      className="tour-grid"
+      className="tour-grid mb-12"
     >
       {allowSounds && (
         <Suspense fallback={null}>
